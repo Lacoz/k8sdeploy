2303fd81eb4036b1fcc9402c9286697ad8b19386
Ladislav Zigo <zigo@profesia.sk>
2020-10-26 12:18:14 +0100

    Add env file

--- phpapache/index.php
+++ phpapache/index.php
@@ -1,10 +1,17 @@
 <?php
+try {
+    require_once 'vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    
+    $stdout = fopen('php://stdout', 'w');
+    
+    print("HOSTNAME: ". gethostname() . ", ELASTICSEARCH_HOST: ". $_ENV['ELASTICSEARCH_HOST']);
 
-$stdout = fopen('php://stdout', 'w');
-
-echo "Hello from ". gethostname();
-
-fwrite($stdout, json_encode($_ENV));
-fwrite($stdout, "stdout\n");
-
+    fwrite($stdout, json_encode($_ENV, JSON_PRETTY_PRINT));
+    fclose($stdout);
+} catch (Exception $e) {
+    header("HTTP/1.1 503"); 
+    print($e->getMessage());
+} 
 ?>
